8fbcff5f342000a2998fefb7a16a3c4bfbd08d98
Rashmi Shukla <radave@microsoft.com>
2021-09-12 18:33:13 +0000

    Deleted Teradata to Azure SQL DW Scripts

--- IP and Scripts/Teradata to Azure SQL DW Scripts/Teradata to Azure SQL DW Scripts/_MiscScripts/mssql-scripterexample.bash
+++
@@ -1,3 +0,0 @@
-#/***This Artifact belongs to the Data SQL Ninja Engineering Team***/
-pip install mssql-scripter
-mssql-scripter -S <servername>.database.windows.net -d <database> -U <user> -P <password> -f ./CreateSQLDWTables.dsql --script-create --target-server-version azuredw --display-progress
