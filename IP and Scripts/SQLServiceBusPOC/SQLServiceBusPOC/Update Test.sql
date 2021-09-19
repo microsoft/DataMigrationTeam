48dfa93164f6b4e098eeee254ae2777c3850152c
Rashmi Shukla <radave@microsoft.com>
2021-09-19 03:56:20 +0000

    Deleted IP and Scripts

--- IP and Scripts/SQLServiceBusPOC/SQLServiceBusPOC/Update Test.sql
+++
@@ -1,2 +0,0 @@
-/***This Artifact belongs to the Data SQL Ninja Engineering Team***/
-update [WideWorldImporters].[Sales].[Orders] set comments='Test firing of CLR trigger on 5 row update', PerfTest=SYSDATETIME() where OrderID between 1 and 5;
